23dba37493f90788e932832092e846f6f6857f25
😂 <you@example.com>
2026-03-21 14:33:10 +0300

    Updated vite.config.ts

--- mtaka-frontend/vite.config.ts
+++ mtaka-frontend/vite.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(({ mode }) => ({
@@ -18,7 +17,7 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  plugins: [react()].filter(Boolean),
+  plugins: [],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
